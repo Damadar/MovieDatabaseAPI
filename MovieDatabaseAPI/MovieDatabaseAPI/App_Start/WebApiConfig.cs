f915054015524e93a1994d1bd3242bdc2454cbea
James Cordrey <twisted.entwined@gmail.com>
2016-02-28 16:05:57 -0500

    Initial Upload of Files

---
+++ MovieDatabaseAPI/MovieDatabaseAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace MovieDatabaseAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            config.Formatters.XmlFormatter.UseXmlSerializer = true;
+        }
+    }
+}
---
+++ MovieDatabaseAPI/MovieDatabaseAPI/Models/Movies.cs
@@ -0,0 +1,226 @@
+﻿
+namespace MovieDatabaseAPI.Models
+{
+    public class Movies
+    {
+        public int IntMovieID
+        {
+            get;
+            set;
+        }
+
+        public string StrActors
+        {
+            get;
+            set;
+        }
+
+        public string StrAwards
+        {
+            get;
+            set;
+        }
+
+        public string StrBoxOffice
+        {
+            get;
+            set;
+        }
+
+        public string StrCountry
+        {
+            get;
+            set;
+        }
+
+        public string StrDirector
+        {
+            get;
+            set;
+        }
+
+        public string StrDVDRelease
+        {
+            get;
+            set;
+        }
+
+        public string StrGenres
+        {
+            get;
+            set;
+        }
+
+        public string StrIMDBID
+        {
+            get;
+            set;
+        }
+
+        public string StrIMDBRating
+        {
+            get;
+            set;
+        }
+
+        public string StrIMDBVotes
+        {
+            get;
+            set;
+        }
+
+        public string StrLanguage
+        {
+            get;
+            set;
+        }
+
+        public string StrMetaScore
+        {
+            get;
+            set;
+        }
+
+        public string StrMovieTitle
+        {
+            get;
+            set;
+        }
+
+        public string StrMovieWebsite
+        {
+            get;
+            set;
+        }
+
+        public string StrPlot
+        {
+            get;
+            set;
+        }
+
+        public string StrPoster
+        {
+            get;
+            set;
+        }
+
+        public string StrPosterURL
+        {
+            get;
+            set;
+        }
+
+        public string StrProductionCompany
+        {
+            get;
+            set;
+        }
+
+        public string StrRated
+        {
+            get;
+            set;
+        }
+
+        public string StrReleaseDate
+        {
+            get;
+            set;
+        }
+
+        public string StrRuntime
+        {
+            get;
+            set;
+        }
+
+        public string StrTomatoConsensus
+        {
+            get;
+            set;
+        }
+
+        public string StrTomatoeURL
+        {
+            get;
+            set;
+        }
+
+        public string StrTomatoFresh
+        {
+            get;
+            set;
+        }
+
+        public string StrTomatoImage
+        {
+            get;
+            set;
+        }
+
+        public string StrTomatoMeter
+        {
+            get;
+            set;
+        }
+
+        public string StrTomatoRating
+        {
+            get;
+            set;
+        }
+
+        public string StrTomatoReviews
+        {
+            get;
+            set;
+        }
+
+        public string StrTomatoRotten
+        {
+            get;
+            set;
+        }
+
+        public string StrTomatoUserMeter
+        {
+            get;
+            set;
+        }
+
+        public string StrTomatoUserRating
+        {
+            get;
+            set;
+        }
+
+        public string StrTomatoUserReviews
+        {
+            get;
+            set;
+        }
+
+        public string StrType
+        {
+            get;
+            set;
+        }
+
+        public string StrWriter
+        {
+            get;
+            set;
+        }
+
+        public string StrYear
+        {
+            get;
+            set;
+        }
+
+        public Movies()
+        {
+        }
+    }
+}
